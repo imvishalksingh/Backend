28fc3fa6261e52ca94689c6c9b9c27700873840c
Vishal Singh <161681190+imvishalksingh@users.noreply.github.com>
2025-08-05 16:31:27 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,37 +1,37 @@
-// require('dotenv').config();
-// const { Pool } = require('pg');
+require('dotenv').config();
+const { Pool } = require('pg');
 
-// // const pool = new Pool({
-// //   connectionString: process.env.DATABASE_URL,
-// //   ssl: {
-// //     rejectUnauthorized: false, // required for Render
-// //   },
-// // });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Render
+  },
+});
 // const pool = new Pool({
 //   connectionString: process.env.DATABASE_URL,
 //   ssl: { rejectUnauthorized: false },
 // });
 
 
-// module.exports = pool;
+module.exports = pool;
 
-const { Pool } = require('pg');
-const dns = require('dns');
-require('dotenv').config();
+// const { Pool } = require('pg');
+// const dns = require('dns');
+// require('dotenv').config();
 
-(async () => {
-  const host = 'db.lltlqduyngqtvflcbjsi.supabase.co'; // Your Supabase DB host
-  const addresses = await dns.promises.resolve4(host); // Only IPv4
+// (async () => {
+//   const host = 'db.lltlqduyngqtvflcbjsi.supabase.co'; // Your Supabase DB host
+//   const addresses = await dns.promises.resolve4(host); // Only IPv4
 
-  const pool = new Pool({
-    host: addresses[0],
-    port: 5432,
-    user: 'postgres',
-    password: process.env.DB_PASSWORD, // or extract from DATABASE_URL
-    database: 'postgres',
-    ssl: { rejectUnauthorized: false }
-  });
+//   const pool = new Pool({
+//     host: addresses[0],
+//     port: 5432,
+//     user: 'postgres',
+//     password: process.env.DB_PASSWORD, // or extract from DATABASE_URL
+//     database: 'postgres',
+//     ssl: { rejectUnauthorized: false }
+//   });
 
-  module.exports = pool;
-})();
+//   module.exports = pool;
+// })();
 
